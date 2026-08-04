ab81555e93fbb31e2310581c6df1d98139408b0b
Fernando Massaru Dos Santos Nishihira <massaru.dev@gmail.com>
2026-08-04 14:09:58 -0400

    Segundo commit

--- App.tsx
+++ App.tsx
@@ -1,28 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
+import '@expo/metro-runtime'
+
 import { StyleSheet, Text, View } from 'react-native';
-// Essa foi a biblioteca que eu instalei para poder usar o React Native com o Expo, e ela é necessária para que o aplicativo funcione corretamente.
-import '@expo/metro-runtime';
 
 export default function App() {
+  // String
+  let nome: string = "Fernando";
+
+  // Number
+  let idade: number = 17;
+
+  // Boolean
+  let status: boolean = true; 
+
+  // Array
+  let frutas: string[] = ["Banana", "Maçã", "Uva"];
+
+  // Tupla
+  let pessoa: [string, number, string] = ["Fernando", 17, "Manaus"];
+
+  // Objeto
+  type Carro = {
+    marca: string,
+    ano: number,
+    cor?: string // A interrogação indica que a propriedade é opcional
+  }
+
+  let carro1 : Carro = {
+    marca: "Fiat",
+    ano: 2020,
+    cor: "Preto"
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Fernando.</Text>
-      <Text style={styles.texto}>Massaru dos Santos.</Text>
-    </View>
+      <View style={styles.alto}>
+        <Text style={styles.texto}>Nome: {nome}</Text>
+        <Text style={styles.texto}>Idade: {idade}</Text>
+      </View>
+
+      <View style={styles.baixo}>
+        <Text style={styles.texto}>Fruta: {frutas[1]}</Text>
+        <Text style={styles.texto}>Marca: {carro1.marca}</Text>
+        <Text style={styles.texto}>{`Marca: ${carro1.marca}`}</Text>
+      </View>
+    </View>    
+    
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  texto: {
+
+  texto:{
     fontSize: 24,
-    color: '#f00',
-    // fontWeight: 'bold',
+    color: "#000"
   },
+
+  baixo: {
+    backgroundColor: '#f8d403',
+    height: 200,
+  },
+
+  alto: {
+    backgroundColor: '#b71bf9',
+    height: 200,
+  }
+
 });
-// Teste GitHub
+
+
